7b7b20ae35fc3f7c4ce13ba8a31cbfab6dee259a
indivar <indivar2.mendiratta@live.uwe.ac.uk>
2025-03-05 14:27:03 +0000

    Front end login and signup

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from myapp.views import home_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('myapp/', include('myapp.urls')),
+    path('', home_view, name="home"),
 ]
