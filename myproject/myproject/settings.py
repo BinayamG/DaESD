912b3c2247ca85f4636359062b0eb75c62f5003c
Binayam Gurung <binayam2.gurung@uwe.ac.uk>
2025-05-08 13:51:45 +0100

    Cleaned structure

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-# Media files (User uploaded files)
+# Media files (User uploaded files post-attachments and profile pictures)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
